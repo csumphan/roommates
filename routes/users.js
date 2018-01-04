406f70285fde505d4d10af731e3f677f4acb2103
Chanun Sumphanphaisal <csumphan@uci.edu>
2018-01-03 19:10:07 -0800

    add a login route

--- routes/users.js
+++ routes/users.js
@@ -17,6 +17,36 @@ router.get('/', function(req, res, next) {
   .catch(error => res.send(error));
 });
 
+router.post('/login', function(req, res, next) {
+  if (!req.body.email || !req.body.email.trim()) {
+    res.status(httpStatus.BAD_REQUEST).json({error: 'ERROR MSG IN PROGRESS'});
+  }
+  if (!req.body.password || !req.body.password.trim()) {
+    res.status(httpStatus.BAD_REQUEST).json({error: 'ERROR MSG IN PROGRESS'});
+  }
+  const body = {
+    email: req.body.email.trim(),
+    password: req.body.password.trim()
+  };
+
+  UserModel.findOne({where: body})
+  .then(user => {
+    if (!user) {
+      res.status(httpStatus.UNAUTHORIZED).json({error: 'Invalid login credentials'});
+      return;
+    }
+
+    const userObj = user.toJSON();
+    const jwtObj = user.toJWTPayload();
+    const token = jwt.createToken(jwtObj);
+
+    userObj.jwt = token;
+
+    res.status(httpStatus.ACCEPTED).json({data: userObj});
+  })
+  .catch(error => res.send(error));
+});
+
 router.post('/', function(req, res, next) {
   if (!req.body.email || !req.body.email.trim()) {
     res.status(httpStatus.BAD_REQUEST).json({error: 'ERROR MSG IN PROGRESS'});
